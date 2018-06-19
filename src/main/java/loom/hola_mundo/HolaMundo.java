0ade8b96486e7798d40c305f9a3182251c330e16
pokengineer <vfacuu@gmail.com>
2018-06-19 17:46:31 -0300

    Update HolaMundo.java

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -3,7 +3,7 @@ package loom.hola_mundo;
 //clase holaMundo
 public class HolaMundo {
 
-	//metodo que tengo que sobreescribir
+//metodo que tengo que sobreescribir
     public String saludar(final String persona) {
         return  "¡Hola, " + persona + "!";
     }
